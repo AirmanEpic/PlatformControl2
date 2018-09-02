226212f64f6f6e0bad0f7664baceadcdc77b0c62
Stefan Blandin <stefanblandin@yahoo.com>
2018-09-02 16:12:32 -0700

    Bluetooth control
    Making the bluetooth be controllable.

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 var main=function(){
 	$('.connectbutton').click(function(event) {
 		//callback hell time
-		bluetoothSerial.list(function(devices){
-			for (var i in devices)
-			{
-				alert(devices[i].name)
-			}
-		})
+		bluetoothSerial.list(devicelist(devices))
+	});
+}
+
+function devicelist(devices){
+	for (var i in devices)
+	{
+		if (devices[i].name=="HC-06")
+		{
+			alert("detected on I "+i)
+
+			result = devices[i].id;
+
+			bluetoothSerial.connect(result,on_connect_success())
+		}
+	}
+}
+
+function on_connect_success(){
+	alert("Successful connection");
+
+	$('.trackbutton').click(function(event) {
+		bluetoothSerial.send("t")
+	});
+
+	$('.revbutton').click(function(event) {
+		bluetoothSerial.send("r")
 	});
 }
 
